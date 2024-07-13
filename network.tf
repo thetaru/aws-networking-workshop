8a43ff96ce2607476d4d09dc2cd1f9ac84fa324e
thetaru <sensesensesense012345678910@gmail.com>
2024-07-13 10:23:06 +0000

    VPC Fundamentals - VPC Endpoints

--- network.tf
+++ network.tf
@@ -169,3 +169,37 @@ resource "aws_nat_gateway" "VPC_A_NATGW" {
 
   depends_on = [aws_internet_gateway.VPC_A_IGW]
 }
+
+# VPC Endpoint
+resource "aws_vpc_endpoint" "VPC_A_KMS_Endpoint" {
+  vpc_id            = aws_vpc.VPC_A.id
+  service_name      = "com.amazonaws.ap-northeast-1.kms"
+  vpc_endpoint_type = "Interface"
+
+  subnet_ids = [
+    aws_subnet.VPC_A_Private_Subnet_AZ1.id,
+    aws_subnet.VPC_A_Private_Subnet_AZ2.id,
+  ]
+
+  private_dns_enabled = true
+
+  tags = {
+    Name = "VPC A KMS Endpoint"
+  }
+}
+
+resource "aws_vpc_endpoint" "VPC_A_S3_Endpoint" {
+  vpc_id            = aws_vpc.VPC_A.id
+  service_name      = "com.amazonaws.ap-northeast-1.s3"
+  vpc_endpoint_type = "Gateway"
+
+  route_table_ids  = [
+    aws_route_table.VPC_A_Public_Route_Table.id,
+    aws_route_table.VPC_A_Private_Route_Table.id,
+    aws_vpc.VPC_A.default_route_table_id,
+  ]
+
+  tags = {
+    Name = "VPC A S3 Endpoint"
+  }
+}
