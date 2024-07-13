550d9fba6e468c5d738d5003a58635505d0447a2
thetaru <sensesensesense012345678910@gmail.com>
2024-07-13 17:20:35 +0000

    Multiple VPCs - TGW Route Tables

--- tgw.tf
+++ tgw.tf
@@ -20,6 +20,8 @@ resource "aws_ec2_transit_gateway_vpc_attachment" "VPC_A_Attachment" {
     aws_subnet.VPC_A_TGW_Subnet_AZ2.id,
   ]
 
+  transit_gateway_default_route_table_association = false
+
   tags = {
     Name = "VPC A Attachment"
   }
@@ -32,6 +34,8 @@ resource "aws_ec2_transit_gateway_vpc_attachment" "VPC_B_Attachment" {
     aws_subnet.VPC_B_TGW_Subnet_AZ1.id,
     aws_subnet.VPC_B_TGW_Subnet_AZ2.id,
   ]
+
+  transit_gateway_default_route_table_propagation = false
 
   tags = {
     Name = "VPC B Attachment"
@@ -46,7 +50,32 @@ resource "aws_ec2_transit_gateway_vpc_attachment" "VPC_C_Attachment" {
     aws_subnet.VPC_C_TGW_Subnet_AZ2.id,
   ]
 
+  transit_gateway_default_route_table_propagation = false
+
   tags = {
     Name = "VPC C Attachment"
   }
+}
+
+resource "aws_ec2_transit_gateway_route_table" "Shared_Services_TGW_Route_Table" {
+  transit_gateway_id = aws_ec2_transit_gateway.TGW.id
+
+  tags = {
+    Name = "Shared Services TGW Route Table"
+  }
+}
+
+resource "aws_ec2_transit_gateway_route_table_association" "Shared_Services_TGW_Route_Table_Association_VPC_A_Attachment" {
+  transit_gateway_attachment_id  = aws_ec2_transit_gateway_vpc_attachment.VPC_A_Attachment.id
+  transit_gateway_route_table_id = aws_ec2_transit_gateway_route_table.Shared_Services_TGW_Route_Table.id
+}
+
+resource "aws_ec2_transit_gateway_route_table_propagation" "Shared_Services_TGW_Route_Table_Propagation_VPC_B_Attachment" {
+  transit_gateway_attachment_id  = aws_ec2_transit_gateway_vpc_attachment.VPC_B_Attachment.id
+  transit_gateway_route_table_id = aws_ec2_transit_gateway_route_table.Shared_Services_TGW_Route_Table.id
+}
+
+resource "aws_ec2_transit_gateway_route_table_propagation" "Shared_Services_TGW_Route_Table_Propagation_VPC_C_Attachment" {
+  transit_gateway_attachment_id  = aws_ec2_transit_gateway_vpc_attachment.VPC_C_Attachment.id
+  transit_gateway_route_table_id = aws_ec2_transit_gateway_route_table.Shared_Services_TGW_Route_Table.id
 }
