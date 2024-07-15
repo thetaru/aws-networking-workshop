001591bb7695378ddd82122cf8b81d7e52e7c704
thetaru <sensesensesense012345678910@gmail.com>
2024-07-15 18:01:29 +0000

    VPC BとVPC CがオンプレVPCに接続できるようにする

--- tgw.tf
+++ tgw.tf
@@ -20,8 +20,6 @@ resource "aws_ec2_transit_gateway_vpc_attachment" "VPC_A_Attachment" {
     aws_subnet.VPC_A_TGW_Subnet_AZ2.id,
   ]
 
-  transit_gateway_default_route_table_association = false
-
   tags = {
     Name = "VPC A Attachment"
   }
@@ -35,8 +33,6 @@ resource "aws_ec2_transit_gateway_vpc_attachment" "VPC_B_Attachment" {
     aws_subnet.VPC_B_TGW_Subnet_AZ2.id,
   ]
 
-  transit_gateway_default_route_table_propagation = false
-
   tags = {
     Name = "VPC B Attachment"
   }
@@ -49,8 +45,6 @@ resource "aws_ec2_transit_gateway_vpc_attachment" "VPC_C_Attachment" {
     aws_subnet.VPC_C_TGW_Subnet_AZ1.id,
     aws_subnet.VPC_C_TGW_Subnet_AZ2.id,
   ]
-
-  transit_gateway_default_route_table_propagation = false
 
   tags = {
     Name = "VPC C Attachment"
@@ -104,11 +98,6 @@ resource "aws_ec2_transit_gateway_route_table_association" "VPN_Route_Table_Asso
 
 # TGW Route Table Propagation
 resource "aws_ec2_transit_gateway_route_table_propagation" "Default_TGW_Route_Table_Propagation_VPC_A_Attachment" {
-  transit_gateway_attachment_id  = aws_vpn_connection.OnPremise_VPN_Connection.transit_gateway_attachment_id
-  transit_gateway_route_table_id = aws_ec2_transit_gateway_route_table.Default_TGW_Route_Table.id
-}
-
-resource "aws_ec2_transit_gateway_route_table_propagation" "Default_TGW_Route_Table_Propagation_OnPremise_VPC_Attachment" {
   transit_gateway_attachment_id  = aws_ec2_transit_gateway_vpc_attachment.VPC_A_Attachment.id
   transit_gateway_route_table_id = aws_ec2_transit_gateway_route_table.Default_TGW_Route_Table.id
 }
@@ -136,6 +125,12 @@ resource "aws_ec2_transit_gateway_route_table_propagation" "VPN_Route_Table_Prop
 resource "aws_ec2_transit_gateway_route_table_propagation" "VPN_Route_Table_Propagation_VPC_C_Attachment" {
   transit_gateway_attachment_id  = aws_ec2_transit_gateway_vpc_attachment.VPC_C_Attachment.id
   transit_gateway_route_table_id = aws_ec2_transit_gateway_route_table.VPN_Route_Table.id
+}
+
+resource "aws_ec2_transit_gateway_route" "Default_TGW_Route_Table_Routes_OnPremise_VPC" {
+  destination_cidr_block         = "172.16.0.0/16"
+  transit_gateway_attachment_id  = aws_vpn_connection.OnPremise_VPN_Connection.transit_gateway_attachment_id
+  transit_gateway_route_table_id = aws_ec2_transit_gateway_route_table.Default_TGW_Route_Table.id
 }
 
 resource "aws_ec2_transit_gateway_route" "Shared_Services_TGW_Route_Table_Routes_OnPremise_VPC" {
